0c870c778f6b01e1529dce812f6d096c05cd7410
JimiStephen <luxideyao@163.com>
2019-04-27 22:21:06 +0800

    添加学习起始项目

---
+++ tutorials-core/src/main/java/com/jimi/springframework/tutorialscore/TutorialsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jimi.springframework.tutorialscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialsCoreApplication.class, args);
+	}
+
+}
